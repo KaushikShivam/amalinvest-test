499f7f2c16d17adb939cfa2a615e181d7be19249
Shivam Kaushik <shivam.kaushik@outlook.com>
2022-11-20 21:04:17 +0530

    refactor(app): Add logic to correctly caculate shares performance

--- backend/src/stock/stock.service.ts
+++ backend/src/stock/stock.service.ts
@@ -1,6 +1,7 @@
 import { Stock, StockWithWeight } from '@amalinvest/types';
 import {
   BadRequestException,
+  flatten,
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
@@ -39,6 +40,8 @@ export class StockService {
     if (!process.env.POLYGON_KEY)
       throw new InternalServerErrorException('Polygon key not found');
     try {
+      const SUM_INVESTED = 100;
+
       // Get starting and ending period
       const today = dayjs().format('YYYY-MM-DD');
       const lastYear = dayjs().subtract(1, 'year').format('YYYY-MM-DD');
@@ -51,29 +54,59 @@ export class StockService {
       );
       const responses = await axios.all(promises);
 
+      // Set all trading dates to performance object
+      const performanceObj: { [key: string]: number } = {};
+      const allDates = flatten(
+        responses.map((i) => i.data.results.map((i) => i.t.toString())),
+      ).sort();
+      const avgTradingStartIndex = Object.keys(AVG_MARKET_PERFORMANCE).indexOf(
+        allDates[0],
+      );
+      // Remove all SPY trading days that are before the performance starting period
+      // Add tradings days after the SPY ending period
+      const tradingDays = Array.from(
+        new Set([
+          ...Object.keys(AVG_MARKET_PERFORMANCE).slice(avgTradingStartIndex),
+          ...allDates,
+        ]),
+      );
+      tradingDays.forEach((date) => (performanceObj[date] = 0));
+
+      const allotedShares: { [key: string]: number } = {};
+
       const resultsObj: { [key: string]: { [key: string]: number } } = {};
       responses.forEach((response) => {
+        // Calculate allocated shares
+        const stock = stocks.find((i) => i.stock === response.data.ticker);
+        allotedShares[response.data.ticker] =
+          (SUM_INVESTED * (parseFloat(stock.weight) / 100)) /
+          response.data.results[0].c;
+
         resultsObj[response.data.ticker] = {};
         response.data.results.forEach((value) => {
           resultsObj[response.data.ticker][value.t] = value.c;
         });
       });
 
-      // Find the dates from the stock that has the most available dates;
-      const stockDates = responses
-        .sort((a, b) => b.data.results.length - a.data.results.length)[0]
-        .data.results.map((i) => i.t);
-      // Set all available dates to performance object
-      const performanceObj: { [key: string]: number } = {};
-      stockDates.forEach((key) => (performanceObj[key] = 0));
-
       // Use average performance value if the stock doesn't have data on specific date.
       Object.entries(performanceObj).forEach(([key, _]) => {
-        stocks.forEach(({ stock, weight }) => {
+        stocks.forEach(({ stock }) => {
           const result = resultsObj[stock];
+
+          if (result[key]) {
+            performanceObj[key] += result[key] * allotedShares[stock];
+          } else {
+            const index = Object.keys(result).indexOf(key);
+            const previousTrades = Object.entries(result).slice(0, index);
+            // Use closing price from previous day, else use avg market performance
+            performanceObj[key] += previousTrades.length
+              ? previousTrades.pop()[1]
+              : AVG_MARKET_PERFORMANCE[key] * allotedShares[stock];
+          }
+
           performanceObj[key] += result[key]
-            ? result[key] * (parseFloat(weight) / 100)
-            : AVG_MARKET_PERFORMANCE[key] * (parseFloat(weight) / 100);
+            ? result[key] * allotedShares[stock]
+            : AVG_MARKET_PERFORMANCE[key] * allotedShares[stock];
         });
       });
 
